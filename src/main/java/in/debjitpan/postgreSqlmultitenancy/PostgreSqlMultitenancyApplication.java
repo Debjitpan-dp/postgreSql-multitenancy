6f357530fe7d853387bc12d77fd94763fe16320a
Debjit Pan <debjitpan.dp@gmail.com>
2024-05-19 00:56:39 +0530

    Initial commit

---
+++ src/main/java/in/debjitpan/postgreSqlmultitenancy/PostgreSqlMultitenancyApplication.java
@@ -0,0 +1,13 @@
+package in.debjitpan.postgreSqlmultitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlMultitenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlMultitenancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/debjitpan/postgreSqlmultitenancy/PostgreSqlMultitenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package in.debjitpan.postgreSqlmultitenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqlMultitenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
